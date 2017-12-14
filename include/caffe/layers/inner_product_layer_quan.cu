4fd24e728321ca92180616a1468c06e03e54d3d7
luolongqiang <lqluo@whu.edu.cn>
2017-12-14 19:20:16 +0800

    Create inner_product_layer_quan.cu

---
+++ include/caffe/layers/inner_product_layer_quan.cu
@@ -0,0 +1,13 @@
+#ifndef CAFFE_INNER_PRODUCT_LAYER_HPP_
+#define CAFFE_INNER_PRODUCT_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+/* added start */
+extern int global_top_scale;
+extern int global_bias_scale;
+/* sdded end */
